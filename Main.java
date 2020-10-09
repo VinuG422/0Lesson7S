39dfc4e5ce0f22c0562a56072064d4b4102248f2
Vinu <vinudn@yahoo.com>
2020-10-08 23:24:54 -0500

    Game

--- Main.java
+++ Main.java
@@ -1,7 +1,26 @@
-public class Main{
-	public static void main(String[] args){
+import javax.swing.JFrame;
 
+import view.GameBoard;
 
+public class Main {
+	
 		
+			public static void main(String[] args){
+				JFrame window = new JFrame();
+				window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				window.setTitle("Space Invader");
+				window.setLocation(500, 100);
+		
+				var menu = new GameBoard(window);
+				menu.init();
+				
+				window.setResizable(false);
+				window.pack();
+				window.setVisible(true);
+		
+
+
+
+
 	}
 }
